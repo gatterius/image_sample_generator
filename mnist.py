dd7c97ff8a275a3161405ca6da989bc21fa95bf3
PPankin <ppankin@croc.ru>
2020-01-12 21:21:33 +0300

    An example of usage (with MNIST digits) was added.

---
+++ mnist.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+from classes.ImageSampleGenerator import ImageSampleGenerator
+
+
+def main():
+    ((train_data, train_labels), (eval_data, eval_labels)) = tf.keras.datasets.mnist.load_data()
+    IG = ImageSampleGenerator('data/paper_photo', 'data/test_gen/', train_data, train_labels, 28, 28)
+    IG.generate_dataset(20)
+
+
+if __name__ == "__main__":
+    main()
+
